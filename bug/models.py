6b35be3f8ac6ca6e60a1bf789a95d439c317b313
Nali H <nalitah1@gmail.com>
2023-10-06 15:54:18 -0400

    added db to Bug app (T347254)

--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Bug(models.Model):
+    description_text = models.CharField(max_length=200)
+    bug_type_text = models.CharField(max_length=200)
+    report_date = models.DateTimeField()
+    status_text = models.CharField(max_length=50)
+
+    def was_reported_recently(self):
+        return self.report_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.bug_type_text
